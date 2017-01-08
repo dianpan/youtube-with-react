2f594e60b3a48440494fd1644bef82fa37f73608
Dian Pan <dian.di.pan@gmail.com>
2017-01-08 15:25:40 -0800

    Make the search input a controlled component
    its value only changes when its state changes

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 
-//all class-based components must have a render function
 class SearchBar extends Component {
-  //initializer method on the class
   constructor(props) {
     super(props);
-    //state exists on any class-component
     this.state = { term: '' };
   }
 
   render() {
-    //onChange is a special React property
     return (
       <div>
-        <input onChange = { event => this.setState({ term: event.target.value }) } />
+        <input
+          value = { this.state.term }
+          onChange = { event => this.setState({ term: event.target.value }) }
+        />
       </div>
     );
   }
