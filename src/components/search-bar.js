e4886649789fc5a241558bb8f8267dfac033089d
Dian Pan <dian.di.pan@gmail.com>
2017-01-08 15:02:10 -0800

    add user event handler to search bar

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 
+//all class-based components must have a render function
 class SearchBar extends Component {
   render() {
-    return <input />;
+    return <input onChange = { event => console.log(event.target.value) } />;
+    //onChange is a special React property
   }
 }
 
