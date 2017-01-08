008aeb51158ec5820b29fec4987248913beecfbf
Dian Pan <dian.di.pan@gmail.com>
2017-01-08 15:22:43 -0800

    the search-bar component re-renders when state changes

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 
 //all class-based components must have a render function
 class SearchBar extends Component {
+  //initializer method on the class
+  constructor(props) {
+    super(props);
+    //state exists on any class-component
+    this.state = { term: '' };
+  }
+
   render() {
-    return <input onChange = { event => console.log(event.target.value) } />;
     //onChange is a special React property
+    return (
+      <div>
+        <input onChange = { event => this.setState({ term: event.target.value }) } />
+      </div>
+    );
   }
 }
 
